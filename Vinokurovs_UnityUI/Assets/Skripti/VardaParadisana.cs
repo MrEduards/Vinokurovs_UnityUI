d4dec69636300b030ba8011ae29b6168ca1d432b
E?V <liepa003@inbox.lv>
2023-04-24 22:49:15 +0300

    vards un vecuma ievade

---
+++ Vinokurovs_UnityUI/Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+public class VardaParadisana : MonoBehaviour {
+
+
+
+	public string teksts;
+
+	public GameObject ievadesLauks;
+
+	public GameObject tekstaAttelosana;
+
+	public void uzglabaTekstu(){
+
+		teksts = ievadesLauks.GetComponent<Text>().text;
+		tekstaAttelosana.GetComponent<Text>().text = "Roblox varonis "+teksts.ToUpper ();
+	}
+}
